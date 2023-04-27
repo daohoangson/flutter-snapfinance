7fe3a046207bae70ffab003cf46ebfa4d78e54f8
Dao Hoang Son <daohoangson@gmail.com>
2023-04-27 17:44:37 +0700

    Clean up imports

--- lib/screens/snap/widgets/snap_icons.dart
+++ lib/screens/snap/widgets/snap_icons.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:snapfinance/widgets/icons/user_icon.dart';
 import 'package:snapfinance/widgets/nope.dart';
 
--- lib/theme.dart
+++ lib/theme.dart
@@ -1,7 +1,5 @@
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 const scheme = FlexScheme.amber;
 final theme = FlexThemeData.light(scheme: scheme);
